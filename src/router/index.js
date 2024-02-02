b7f23eff861e35848f78de3a909273a574315ebd
exzolink <exzolink@gmail.com>
2024-02-02 09:21:54 +0300

    Moved to docs directory

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router"
 import HomeView from "@/views/HomeView.vue"
 
 const router = createRouter({
-  history: createWebHashHistory("/AllodsArmory/dist/"),
+  history: createWebHashHistory("/AllodsArmory/"),
   routes: [
     {
       path: "/",
